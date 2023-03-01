f578c9ff28e11e4de6d7723731904897a961acb6
joao-hideki <joaoohideki12345@gmail.com>
2023-02-28 23:23:01 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello world!");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+app.listen(port, () => console.log(`listening on port ${port}`));
